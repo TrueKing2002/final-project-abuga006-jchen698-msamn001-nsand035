ea805d7d1dee54b961062d2b7ef51a1590742f89
Megansamn <msamn001@ucr.edu>
2022-02-25 13:16:05 -0800

    add Computer_move()

--- ChessGame.cpp
+++ ChessGame.cpp
@@ -8,3 +8,20 @@ void ChessGame::printBoard() {
 	if (playerIsWhite) theBoard->printBoardWhite();
         else theBoard->printBoardBlack();
 }
+
+void ChessGame::computerMove() {
+    srand(time(0));
+    int randnum1 = rand() % 64; 
+    ChessPiece* randomPiece = theBoard[randnum1];
+
+    while ((playerIsWhite && randomPiece->white == true) || (!playerIsWhite && randomPiece->white == false)) { 
+    	randnum1 = rand() % 64;
+    }                
+    
+    int randomDest = rand() % 64;
+    
+    while (!randomPiece->canMove(theBoard, randomDest)) {
+	randomDest = rand() % 64;
+    }
+    move(randnum1, randomDest); 
+}    
