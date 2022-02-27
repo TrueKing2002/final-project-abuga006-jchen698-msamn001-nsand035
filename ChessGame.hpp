219939ea914db24e7937b37a6495077803f097e9
TrueKing2002 <2002theking2002@gmail.com>
2022-02-27 12:53:48 -0800

    Upgraded move() and main to allow testing of canMove functions
    Also added stack to store game moves.

--- ChessGame.hpp
+++ ChessGame.hpp
@@ -2,17 +2,21 @@
 #define CHESSGAME
 
 #include "board/ChessBoard.cpp"
+#include <stack>
 
 class ChessGame {
     private:
 	ChessBoard* theBoard;
         bool playerIsWhite = true;
+	stack<int> moveLog;
     public:
 	ChessGame() {theBoard = new ChessBoard;}
         ~ChessGame() {delete theBoard;}
         void setPlayerWhite() {playerIsWhite = true;}
         void setPlayerBlack() {playerIsWhite = false;}
-        void move(int, int);
+        bool move(int, int);
+	void computerMove();
+	int inCheck();
         void printBoard();
 };
 
