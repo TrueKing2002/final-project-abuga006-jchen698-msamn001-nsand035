84f91c998bea205e5082fe05ee835743305fe7f7
Nick <njsandburg@gmail.com>
2022-02-28 15:43:10 -0800

    Added updown, leftright, diagonal

--- pieces/King.cpp
+++ pieces/King.cpp
@@ -5,36 +5,42 @@ class King : public ChessPiece {
         if (white) return "♚";
         else return "♔";
     }
+
+	bool UpDown(ChessPiece** board, int dest, int location) {
+		if (board[location + 8] != nullptr) {
+			// if there is a piece, return false
+			return false;
+		}
+		else if (dest == location + 8) {
+			return true;
+		}
+
+		if (board[location - 8] != nullptr) {
+			return false;
+		}
+		else if (dest == location - 8) {
+			return true;
+		}
+		return false;
+	}
     
-    bool UpDown(ChessPiece** board, int dest, int location) {
+    bool LeftRight(ChessPiece** board, int dest, int location) {
 		// white pieces
 		if (white) {
-			//check above location to see if there is a piece there
+			//check location to the right to see if there is a piece there
 			if (board[location + 1] != nullptr) {
-				// check if the piece above is the same color, if so return false
-				if (board[location + 1]->white) {
-					return false;
-				}
-				// piece is not our color, and we can capture it
-				// set the location to null, so we can move there
-				board[location + 1] == nullptr;
-				return true;
+				// if there is a piece, return false
+				return false;
 			}
 			// there is no piece above so we can move there
 			else if (dest == location + 1) {
 				return true;
 			}
 
-			// check location below to see if there is a piece there
+			// check location to the left to see if there is a piece there
 			if (board[location - 1] != nullptr) {
-				// check if the piece below is the same color, if so return false
-				if (board[location - 1]->white) {
-					return false;
-				}
-				// piece is not our color, and we can capture it
-				// set the location to null, so we can move there
-				board[location - 1] == nullptr;
-				return true;
+				// if there is a piece, return false
+				return false;
 			}
 			// there is no piece above so we can move there
 			else if (dest == location + 1) {
@@ -76,9 +82,49 @@ class King : public ChessPiece {
 		return false;
 	}
 
+	bool Diagonal(ChessPiece** board, int dest, int location) {
+		// up right
+		if (board[location + 9] != nullptr) {
+			return false;
+		}
+		else {
+			return true;
+		}
+
+		// up left
+		if (board[location + 7] != nullptr) {
+			return false;
+		}
+		else {
+			return true;
+		}
+
+		// down right
+		if (board[location - 7] != nullptr) {
+			return false;
+		}
+		else {
+			return true;
+		}
+
+		// down left
+		if (board[location - 9] != nullptr) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
 	bool canMove(ChessPiece** board, int dest, int location) {
 		dest = trans(dest);
-		if (UpDown(board, dest, location)) {
+
+		// if the square is not on the board, return false
+		if (dest < 1 || dest > 64) {
+			return false;
+		}
+
+		if (UpDown(board, dest, location)) || LeftRight(board, dest, location) || Diagonal(board, dest, location) {
 			return true;
 		}
 		return false;
