e2becd11195c7d15862e3f2eab4d04661834d4e1
jc51001 <jchen698@ucr.edu>
2022-02-28 16:49:18 -0800

    Added Basic Bishop class, still need to account for pieces in the way

--- pieces/Bishop.cpp
+++ pieces/Bishop.cpp
@@ -9,5 +9,52 @@ public:
         else return "♗";
     }
     
-    bool canMove(ChessPiece** board, int dest) {return false;}
+    bool canMove(ChessPiece** board, int dest) {
+	dest = trans(dest);
+
+	if (board[dest] != nullptr) {
+	    if (white) {
+		if (board[dest]->white) {
+		    return false;
+		}
+	    }
+	    if (black) {
+		if (board[dest]->black) {
+		    return false;
+		}
+	    }
+	}
+	
+	int inColumn;
+	int inRow;
+	int column;
+	int row;
+
+	if (location % 8 == 0) { //If initial on last column
+	     inColumn = 8; //Will be in column h
+	     inRow = location / 8; //Don't need to add 1 since it is divisible by 8
+	}
+	else { //General coordinates of initial
+	     inColumn = location % 8; //All other columns except h
+	     inRow = location / 8 + 1; //All rows when not in column h
+	}
+
+	if (dest % 8 == 0) { //If destination on last column
+	     column = 8; //Will be in column h
+	     row = dest / 8; //Don't need to add 1 since it is divisible by 8;
+	}
+	else { //General coordinates of destination
+	     column = dest % 8; //All other columns except h
+	     row = dest / 8 + 1; //All rows when not in column h
+	}
+
+	int validCheck = (row - inRow) - (column - inColumn); //Checks difference in row and column between initial and destination positions
+
+	if (validCheck == 0) {
+	    return true;
+	}
+
+	return false;
+    }
+
 };
