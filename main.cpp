5b06ebd41bcca9e6034eab7cad2dfd7d6e035785
jc51001 <jchen698@ucr.edu>
2022-03-05 20:08:07 -0800

    Updated Knight Class, should function properly now

--- main.cpp
+++ main.cpp
@@ -10,55 +10,22 @@ int main() {
     game.printBoard();
     
     game.setPlayerWhite();  // Changes player back to white
-    cout << "\nMOVE b1 a3 (2, 17)\n";
-    game.move(2, 17);
+    cout << "\nMOVE e2 e4 (13, 29)\n";
+    game.move(13, 29);
     game.printBoard();
 
-    cout << "\nOPPONENT MOVE a3 h5 (17, 40)\n";
-    game.move(17, 40);
+    cout << "\nOPPONENT MOVE d7 d5 (52, 36)\n";
+    game.move(52, 36);
     game.printBoard();
 
-    cout << "\nMOVE a3 g4 (17, 31)\n";
-    game.move(17, 31);
-    game.printBoard();
-
-    cout << "\nMOVE g1 f3 (7, 22)\n";
-    game.move(7, 22);
-    game.printBoard();
-
-    cout << "\nMOVE f3 h4 (22, 32)\n";
-    game.move(22, 32);
-    game.printBoard();
-
-/*
-    cout << "\nMOVE g1 h3 (7, 24)\n";
-    game.move(7, 24);
-    game.printBoard();
-
-    cout << "\nMOVE h3 a5 (24, 33)\n";
-    game.move(24, 33);
-    game.printBoard();
-
-    cout << "\nMOVE h3 b4 (24, 26)\n";
-    game.move(24, 26);
-    game.printBoard();
-
-    cout << "\nMOVE b2 b4 (10, 26)\n";
-    game.move(10, 26);
-    game.printBoard();
-
-    cout << "\nMOVE b4 b5 (26, 34)\n";
-    game.move(26, 34);
-    game.printBoard();
-
-    cout << "\nMOVE a3 b5 (17, 34)\n";
-    game.move(17, 34);
+    cout << "\nMOVE b1 c3 (2, 19)\n";
+    game.move(2, 19);
     game.printBoard();
 
     cout << "\nPRINT BOARD BLACK: \n";
     game.setPlayerBlack();
-    game.printBoard(); */
-/*
+    game.printBoard();
+
     for (int i = 1; i < 20; i++) { // You can change the loop to run as many times as you want
         cout << "Computer Move " << i << " (White)" << endl;
 	game.setPlayerBlack();
@@ -70,6 +37,6 @@ int main() {
         game.computerMove();
         game.printBoard();
     }
-*/
+
     return 1;
 }
--- pieces/Knight.cpp
+++ pieces/Knight.cpp
@@ -32,21 +32,19 @@ public:
 
 	if (location % 8 == 0) { //If initial on last column
 	     inColumn = 8; //Will be in column h
-	     inRow = location / 8; //Don't need to add 1 since it is divisible by 8
 	}
 	else { //General coordinates of initial
-	     inColumn = location % 8; //All other columns except h
-	     inRow = location / 8 + 1; //All rows when not in column h
+	     inColumn = trans(location % 8); //All other columns except h
 	}
+	inRow = location / 8 + 1; //All rows when not in column h
 
 	if (dest % 8 == 0) { //If destination on last column
 	     column = 8; //Will be in column h
-	     row = dest / 8; //Don't need to add 1 since it is divisible by 8;
 	}
 	else { //General coordinates of destination
-	     column = dest % 8; //All other columns except h
-	     row = dest / 8 + 1; //All rows when not in column h
+	     column = trans(dest % 8); //All other columns except h
 	}
+	row = dest / 8 + 1; //All rows when not in column h
 	
 	if (column - inColumn == 1 || column - inColumn == -1) {
 	    if (row - inRow == 2 || row - inRow == -2) {
