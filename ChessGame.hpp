cce36647408a51b9a0b149da393a310dc5d90406
TrueKing2002 <50130189+TrueKing2002@users.noreply.github.com>
2022-06-13 21:12:15 -0700

    Updated to newer version

--- ChessGame.hpp
+++ ChessGame.hpp
@@ -3,39 +3,45 @@
 
 #include "board/ChessBoard.cpp"
 #include <stack>
+#include <vector>
+#include <time.h>
 
 class ChessGame {
     private:
-	ChessBoard* theBoard;
-        bool playerIsWhite = true;
-	stack<int> moveLog;
-	void announce(int);
-	int randomSeed = 0;
-	ChessPiece* retrievePiece(int);
-	bool whiteMate();
-	bool blackMate();
+        ChessBoard* theBoard;
+        bool playerIsWhite() {return currentPlayer;}
+        stack<int> moveLog;
+        int randomSeed = 0;
+        void announce(int);
+        ChessPiece* retrievePiece(int);
+        bool canCheck();
+        bool whiteMate();
+        bool blackMate();
+        bool onlyKings();
+	
     public:
-	ChessGame() {theBoard = new ChessBoard;}
+        ChessGame() {theBoard = new ChessBoard;}
         ~ChessGame() {delete theBoard;}
-	bool currentPlayer = 1;
-	bool opponentIsComputer = true;
-	bool sameColor(int);
-        void setPlayerWhite() {playerIsWhite = true;}
-        void setPlayerBlack() {playerIsWhite = false;}
-	void swapPlayer();
+        bool currentPlayer = 1;
+        bool opponentIsComputer = true;
+        void setPlayerWhite() {currentPlayer = 1;}
+        void setPlayerBlack() {currentPlayer = 0;}
+        void swapPlayer();
         bool move(int, int);
+        void computerMove();
+        bool sameColor(int);
         void printBoard();
-	void computerMove();
-	void announceMove(int, int);
-	void undoMoves();
-	void undo();
-	bool playerInCheck();
-	bool playerInMate();
-	bool canPromote();
-	void promotePawn(int);
-	void announceCheck();
-	bool announceMate();
-	bool allowCheats = true; // Undos and random (computer) moves ON by default
+        void announceMove(int, int);
+        void undo();
+        bool undoMoves(int);
+        bool playerInCheck();
+        bool playerInMate();
+        void announceCheck();
+        int announceMate();
+        bool canPromote();
+        void promotePawn(int);
+        void resetBoard() {delete theBoard; theBoard = new ChessBoard;}
+        bool allowCheats = true; // Undos and random (computer) moves ON by default
 };
 
 #endif
